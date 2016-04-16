25a7adf801f2a935aa70b1aff83490e5521a2c13
joeluwu <lzwu@ucsd.com>
2016-04-15 22:14:42 -0700

    Initial Commit

---
+++ PA2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PA2
+//
+//  Created by Joe Lu Wu on 4/15/16.
+//  Copyright © 2016 Joe Lu Wu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
